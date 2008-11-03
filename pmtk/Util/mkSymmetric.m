a7406dd9ab9e01223a58186d4b7d43c1c8cbfc3f
murphyk2@gmail.com <murphyk2@gmail.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-11-03 00:15:06 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@58 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/Util/mkSymmetric.m
+++ pmtk/Util/mkSymmetric.m
@@ -1,3 +1,14 @@
 function M = mkSymmetric(M)
 
-M = (M+M')/2;
+n = size(M,1);
+for i=1:n
+  for j=1:n
+    if M(i,j)==0, M(i,j)=M(j,i);
+    end
+  end
+end
+    
+%T = triu(M);
+%L = tril(M);
+%M =T | T' | L | L';
+%M = (M+M')/2;
