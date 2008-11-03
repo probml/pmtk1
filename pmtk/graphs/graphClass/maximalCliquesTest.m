32a92dd3c390d4fd205f388462702a4b61e487ac
murphyk2@gmail.com <murphyk2@gmail.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-11-03 19:33:52 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@74 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/graphs/graphClass/maximalCliquesTest.m
+++
@@ -1,8 +0,0 @@
-% Whittaker's marks example
-G = zeros(5,5);
-me = 1; ve = 2; al= 3; an = 4; st = 5;
-G([me,ve,al], [me,ve,al]) = 1;
-G([al,an,st], [al,an,st]) = 1;
-G = setdiag(G,0);
-clq = maximalCliques(G)
-
