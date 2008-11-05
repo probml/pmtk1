1958ee8199be31f08dab9eed6e30ed09ad32a1bd
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-11-05 07:01:31 +0000

    Vectorized mkSymmetric.m
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@96 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/Util/mkSymmetric.m
+++ pmtk/Util/mkSymmetric.m
@@ -1,13 +1,20 @@
 function M = mkSymmetric(M)
 
-n = size(M,1);
-for i=1:n
-  for j=1:n
-    if M(i,j)==0, M(i,j)=M(j,i);
-    end
-  end
-end
+%Mtest = M;    
+Mt = M';
+M(M==0) = Mt(M==0);
     
+%     
+% n = size(Mtest,1);
+% for i=1:n
+%   for j=1:n
+%     if Mtest(i,j)==0, Mtest(i,j)=Mtest(j,i);
+%     end
+%   end
+% end
+% assert(isequal(M,Mtest));    
+% 
+
 %T = triu(M);
 %L = tril(M);
 %M =T | T' | L | L';
