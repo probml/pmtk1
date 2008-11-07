a0c349af8be485997121dbe5200a06204afe5695
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-11-07 23:43:02 +0000

    Small bug fix
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@107 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/Lasso/interpolateLarsWeights.m
+++ pmtk/Lasso/interpolateLarsWeights.m
@@ -18,6 +18,8 @@ criticalPoints = recoverLambda(X,y,Wfull)'; %in ascending order of magnitude.
 tooBig = lambdas > criticalPoints(end);%can't interpolate outside of the range of criticalPoints
 Winterp = interp1q(criticalPoints,Wfull,lambdas(~tooBig)');
 Wbig = [Winterp; zeros(sum(tooBig), size(Winterp,2))]; % since, if lambda > lambda_max, all weights 0.
-Wbig = Wbig(1:end-1,:);
-Wbig = [Wbig; Wfull(1,:)];
-
+%Wbig = Wbig(1:end-1,:);
+%Wbig = [Wbig; Wfull(1,:)];
+if(any(lambdas == 0))   % Interpolation breaks when lambda = 0, which corresponds to lsq solution = Wfull(1,:)
+    Wbig(lambdas == 0,:) = Wfull(1,:);
+end
