b9c1e58a65b1fd271f24c4bf8c88e526ec7ecc72
murphyk2@gmail.com <murphyk2@gmail.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-11-28 02:22:45 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@220 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/bookCode/sprinklerMakeJoint.m
+++ pmtk/bookCode/sprinklerMakeJoint.m
@@ -11,37 +11,9 @@ clear all
 C = 1; S = 2; R = 3; W = 4;
 false = 1; true = 2;
 
-% Specify the conditional probability tables as cell arrays
-% The left-most index toggles fastest, so entries are stored in this order:
-% (1,1,1), (2,1,1), (1,2,1), (2,2,1), etc. 
+[G, CPD, joint, nodes] = makeSprinklerBN(true);
 
-CPD{C} = reshape([0.5 0.5], 2, 1);
-CPD{R} = reshape([0.8 0.2 0.2 0.8], 2, 2);
-CPD{S} = reshape([0.5 0.9 0.5 0.1], 2, 2);
-CPD{W} = reshape([1 0.1 0.1 0.01 0 0.9 0.9 0.99], 2, 2, 2);
-
-
-joint = zeros(2,2,2,2);
-for c=1:2
-  for r=1:2
-    for s=1:2
-      for w=1:2
-	joint(c,s,r,w) = CPD{C}(c) * CPD{S}(c,s) * CPD{R}(c,r) * CPD{W}(s,r,w);
-      end
-    end
-  end
-end
-
-joint2 = repmat(reshape(CPD{C}, [2 1 1 1]), [1 2 2 2]) .* ...
-	 repmat(reshape(CPD{S}, [2 2 1 1]), [1 1 2 2]) .* ...
-	 repmat(reshape(CPD{R}, [2 1 2 1]), [1 2 1 2]) .* ...
-	 repmat(reshape(CPD{W}, [1 2 2 2]), [2 1 1 1]);
-assert(approxeq(joint, joint2));
-
-%fac{C} = tabularFactor([C], [2], [0.5 0.5]);
-%fac{R} = tabularFactor([C R], [2 2], [0.8 0.2 0.2 0.8]);
-%fac{S} = tabularFactor([C S], [2 2], [0.5 0.9 0.5 0.1]);
-%fac{W} = tabularFactor([S R W], [2 2 2], [1 0.1 0.1 0.01 0 0.9 0.9 0.99]);
+C = 1; S = 2; R = 3; W = 4;
 
 fac{C} = tabularFactor([C], mysize(CPD{C}), CPD{C});
 fac{R} = tabularFactor([C R], mysize(CPD{R}), CPD{R});
@@ -54,15 +26,3 @@ assert(approxeq(joint, joint3));
 
 dispjoint(joint3)
 
-ndx=ind2subv([2 2 2 2],1:16);
-for i=1:16
-  lab{i} =sprintf('%d ',ndx(i,:));
-end
-% fancy vectorized method:
-lab=cellfun(@(x) {sprintf('%d ',x)}, num2cell(ind2subv([2 2 2 2],1:16),2))
-
-
-figure(1);clf;bar(joint3(:))
-set(gca,'xtick',1:16);
-xticklabelRot(lab, 90, 10, 0.01)
-title('joint distribution of water sprinkler DGM')
--- pmtk/probDists/tabularDistClass/TabularDist.m
+++ pmtk/probDists/tabularDistClass/TabularDist.m
@@ -2,43 +2,57 @@ classdef TabularDist < VecDist
   % tabular (multi-dimensional array) distribution
   
   properties
-    T;
+    Tfac;
   end
   
   %% main methods
   methods
     function m = TabularDist(T)
       if nargin == 0, T = []; end
-      m.T = T;
-      m.stateInfEng = TabularInfer;
+      sz = mysize(T);
+      m.Tfac = TabularFactor(T, 1:length(sz));
     end
 
-    function params = getModelParams(obj)
-      params = {obj.T};
+    function [X] = sample(obj, n)
+       % X(i,:) = sample for i=1:n
+       T = obj.Tfac.T(:);
+       X = sampleDiscrete(T, n, 1);
     end
     
-    function obj = mkRndParams(obj)
-      sz = mysizes(obj.T);
-      obj.T = myreshape(rand(prod(sz),1), sz);
-    end
-    
-    function d = ndims(m)
-     d = ndims(m.T);
+    function d = ndims(obj)
+     d = ndims(obj.Tfac);
     end
   
-    function [mu, m] = mode(m)
-     [mu, m]= mode(m.stateInfEng);
+    function m = mode(obj)
+      m = mode(obj.Tfac);
     end
  
-    function print(m)
-      dispjoint(m.stateInfEng.Tfac.T); % assumes infEng is tabularInfer...
+    function print(obj)
+      dispjoint(obj.Tfac.T);
     end
      
-    function logprob(obj,varargin)
-       error('not yet implemented');  
+    function postQuery = marginal(obj, queryVars)
+      smallpot = marginalize(obj.Tfac, queryVars, false);
+      %postQuery = smallpot;
+      postQuery = TabularDist(smallpot.T);
     end
     
-  
+     function postQuery = predict(obj, visVars, visValues, queryVars)
+      eng.visVars = visVars; eng.visValues = visValues;
+      eng.evidenceEntered = true;
+      % computes p(Xh|Xv=v) and stores internally
+      Fnumer = slice(eng.Tfac, visVars, visValues); % p(H,v)
+      Fnumer = normalizeFactor(Fnumer);
+      eng.Tfac = Fnumer; % sets the domain to hidNodes
+    end
+
+    function [ll, L] = logprob(obj, X)
+      % ll(i) = log p(X(i,:) | params)
+      % L = sum_i ll(i)
+      ndx = subv2ind(obj.Tfac.sizes, X);
+      ll = log(obj.Tfac.T(ndx));
+      L = sum(ll);
+    end
     
   end % methods
 
