ab6fdfaa74be7a9c8268dfad2d13b8a1b6675199
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2008-12-17 21:42:01 +0000

    Removed identical duplicate functions
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@287 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/Util/lookupIndices.m
+++
@@ -1,7 +0,0 @@
-function ndx = lookupIndices(small, big)
-% ndx(i) = location of small(i) in big
-% e.g., small=[8,2], big=[2,4,8,7], ndx = [3 1]
-
-for i=1:length(small)
-  ndx(i) = find(big==small(i));
-end
--- pmtk/Util/reports/deleteIdenticalShadowedCopies.m
+++ pmtk/Util/reports/deleteIdenticalShadowedCopies.m
@@ -1,14 +1,14 @@
-function duplicates = duplicateReport()
-    
+function deleteIdenticalShadowedCopies()
+% Searches through every m-file and deletes exact duplicates.     
     [info,mfiles] = mfilelist(PMTKroot());          %#ok
-    duplicates = {};
+    
     for i=1:numel(mfiles)
         
        w = which('-all',mfiles{i});
        if(numel(w) == 2 && isequal(getText(w{1}),getText(w{2})))
            
-           duplicates = [duplicates;mfiles{i}];
-           which('-all',mfiles{i})
+           fprintf('removing...%s\n',w{2});
+           delete(w{2});
            
        end
         
