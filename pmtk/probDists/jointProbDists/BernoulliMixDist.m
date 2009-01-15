183c794a1bdbd74ffe698612e7b4f126c1c0dd72
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-01-15 00:34:34 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@363 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/probDists/jointProbDists/BernoulliMixDist.m
+++ pmtk/probDists/jointProbDists/BernoulliMixDist.m
@@ -70,6 +70,13 @@ classdef BernoulliMixDist < MixtureDist
              imagesc(reshape(m.distributions{i}.mu(2,:),28,28)); 
            end
            placeFigures('Square',true)
+           if(0)
+              for i=1:numel(lookfor)
+                  figure;
+                  imagesc(reshape(mean(sample(m.distributions{i},500),1),28,28))
+              end
+               placeFigures;
+           end
         end
         
         
