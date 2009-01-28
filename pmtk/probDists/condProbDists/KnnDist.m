ec2f5a0c296cd516c0979c33570fc2cb9d01374f
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-01-28 20:52:32 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@410 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/probDists/condProbDists/KnnDist.m
+++ pmtk/probDists/condProbDists/KnnDist.m
@@ -13,18 +13,20 @@ classdef KnnDist < CondProbDist
         transformer;    % a data transformer object, e.g. PcaTransformer
         localKernel;    % e.g. Gaussian Kernel
         distanceFcn;    % the global metric, e.g. Euclidean distance
+        useSoftMax      % if true, counts are smoothed using the softmax function, with specified inv temp beta
         beta;           % inverse temperate used in softmax smoothing S(y,beta) = normalize(exp(beta*y)
     end
     
     methods
        
         function obj = KnnDist(varargin)
-            [obj.K,obj.transformer,obj.localKernel,obj.distanceFcn,obj.beta] =...
+            [obj.K,obj.transformer,obj.localKernel,obj.distanceFcn,obj.useSoftMax,obj.beta] =...
                 process_options(varargin,...
                 'K'             , 1              ,...
                 'transformer'   , []             ,...
                 'localKernel'   , []             ,...
                 'distanceFcn'   , @sqDistance    ,...
+                'useSoftMax'    , true           ,...
                 'beta'          , 1              );
             
                 if ischar(obj.localKernel)
@@ -51,16 +53,16 @@ classdef KnnDist < CondProbDist
                 weights = obj.localKernel(data,obj.examples,sortedDst,kNearest);
                 counts = zeros(size(data,1),obj.nclasses);
                 for j=1:obj.nclasses
-                    for k=1:obj.K
-                        counts(:,j) = counts(:,j) + (obj.labels(kNearest(:,k)) == j).*weights(:,k);
-                    end
+                    counts(:,j) = counts(:,j) + sum((obj.labels(kNearest) == j).*weights,2);
                 end
-                
             else
                 counts = histc(obj.labels(kNearest),1:obj.nclasses,2);
             end
-                
-            probs = normalize(exp(counts*obj.beta),2);
+            if(obj.useSoftMax)    
+                probs = normalize(exp(counts*obj.beta),2);
+            else
+                probs = normalize(counts,2);
+            end
             pred = DiscreteDist('mu',probs','support',obj.support);
             
         end
@@ -74,9 +76,9 @@ classdef KnnDist < CondProbDist
            switch lower(kernelName)
               
                case 'epanechnikov'
-                   
+                    obj.localKernel = @epanechnikovKernel;
                case 'tricube'
-                   
+                    obj.localKernel = @tricubeKernel;
                case 'gaussian'
                     obj.localKernel = @gaussianKernel;
                     
@@ -85,10 +87,28 @@ classdef KnnDist < CondProbDist
                
                    
            end
-                   function weights = gaussianKernel(testData,examples,sqdist,knearest)
-                       bandwidth = repmatC(sqrt(max(sqdist,[],2))+eps,1,size(knearest,2)); 
-                       weights = normalize(eps + (1./((2*pi)*bandwidth)).*exp(-(sqdist./(2*bandwidth.^2))),2);
-                   end
+            function weights = gaussianKernel(testData,examples,sqdist,knearest)
+                bandwidth = repmatC(sqrt(max(sqdist,[],2))+eps,1,size(knearest,2));
+                weights = normalize(eps + (1./((2*pi)*bandwidth)).*exp(-(sqdist./(2*bandwidth.^2))),2);
+            end
+            
+            function weights = tricubeKernel(testData,examples,sqdist,knearest)
+                bandwidth = repmatC(sqrt(max(sqdist,[],2))+eps,1,size(knearest,2));
+                weights = sqrt(sqdist)./bandwidth;
+                in = abs(weights) <= 1;
+                weights(in)  = (1-weights(in).^3).^3;
+                weights(not(in)) = 0;
+                weights = normalize(eps + weights,2);
+            end
+            
+            function weights = epanechnikovKernel(testData,examples,sqdist,knearest)
+                bandwidth = repmatC(sqrt(max(sqdist,[],2))+eps,1,size(knearest,2));
+                weights = sqrt(sqdist)./bandwidth;
+                in = abs(weights) <= 1;
+                weights(in)  = (3/4)*(1-weights(in).^2);
+                weights(not(in)) = 0;
+                weights = normalize(eps + weights,2);
+            end
                    
                    
            
@@ -121,7 +141,7 @@ classdef KnnDist < CondProbDist
             ytrain = (mnist.train_labels(trainndx));
             ytest  = (mnist.test_labels(testndx));
             clear mnist;
-            model = KnnDist('K',3,'localKernel','Gaussian');
+            model = KnnDist('K',3,'localKernel','gaussian');
             model = fit(model,Xtrain,ytrain);
             clear Xtrain ytrain
             pred = predict(model,Xtest);
