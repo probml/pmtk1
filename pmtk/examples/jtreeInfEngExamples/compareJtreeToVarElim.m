4816c6418653329f515c19c7371486ba3c1341c6
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-02-16 23:37:40 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@500 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/jtreeInfEngExamples/compareJtreeToVarElim.m
+++ pmtk/examples/jtreeInfEngExamples/compareJtreeToVarElim.m
@@ -1,10 +1,10 @@
 %% Test that JtreeInfEng Returns the Same Results as VarElimInfEng
 
-dgmVE = mkAlarmNetworkDgm;
+dgmVE = mkSprinklerDgm;
 dgmJT = dgmVE;
 dgmJT.infEng = JtreeInfEng();
 
-[p1VE,dgmVE] = marginal(dgmVE,4);
-[p1JT,dgmJT] = marginal(dgmJT,4);
+[p1VE,dgmVE] = marginal(dgmVE,1);
+[p1JT,dgmJT] = marginal(dgmJT,1);
 display(pmf(p1VE));
 display(pmf(p1JT));
