eef4f60ab1c6b7557389f4febb28877d5e15ae0c
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-02-17 21:15:22 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@505 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/jtreeInfEngExamples/compareJtreeToVarElim.m
+++ pmtk/examples/jtreeInfEngExamples/compareJtreeToVarElim.m
@@ -1,14 +1,14 @@
 %% Test that JtreeInfEng Returns the Same Results as VarElimInfEng
-
+%#testPMTK
 dgmVE = mkAlarmNetworkDgm;
-%dgmEnum = dgmVE;
-%dgmEnum.infEng = EnumInfEng();
 dgmJT = dgmVE;
 dgmJT.infEng = JtreeInfEng();
 
-[pVE,dgmVE] = marginal(dgmVE,32);
-[pJT,dgmJT] = marginal(dgmJT,32);
-%[pEnum,dgmEnum] = marginal(dgmEnum,[1,2]);
-display(pmf(pVE));
-display(pmf(pJT));
-%display(pmf(pEnum));
+for i=1:37
+    [piVE,dgmVE] = marginal(dgmVE,i);
+    [piJT,dgmJT] = marginal(dgmJT,i);
+    assert(approxeq(pmf(piVE),pmf(piJT)));
+end
+
+
+
