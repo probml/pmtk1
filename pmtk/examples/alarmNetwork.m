bf4c26ad6c12ba8396e86ca20bd4a1775b238146
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-03-17 03:16:24 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@604 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/alarmNetwork.m
+++ pmtk/examples/alarmNetwork.m
@@ -5,7 +5,7 @@
 % merely that the functions can be called...
 dgm = mkAlarmNetworkDgm();
 %dgm.infMethod = 'varElim';
-dgm.infEng = VarElimInfEng('verbose', false);
+dgm.infMethod = VarElimInfEng('verbose', false);
 profile on;
 for i=1:37
     m2 = marginal(dgm, i, mod(i,37)+2, 2);
@@ -15,7 +15,7 @@ end
 profile viewer;
 %% Large Marginal Test
 dgm = mkAlarmNetworkDgm();
-dgm.infEng = VarElimInfEng();
+dgm.infMethod = VarElimInfEng();
 profile on
 m = marginal(dgm,1:20); % 36 million entries!
 %m = marginal(dgm,1:5); 
