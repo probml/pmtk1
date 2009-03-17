e22470a69a234249205bf2290fbb9928cabf387a
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-03-17 01:45:15 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@599 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/inheritedDiseaseVarElim.m
+++ pmtk/examples/inheritedDiseaseVarElim.m
@@ -24,7 +24,7 @@ graph(G3,X3) = 1;
 %Graphlayout('adjMatrix',graph,'nodeLabels',{'G1','G2','G3','X1','X2','X3'})
 
 % Prior on G1
-CPD{G1} = TabularCPD([0.5;0.5]); 
+CPD{G1} = TabularCPD([0.5;0.5]);
 
 % Conditional G2|G1 and G3|G1
 CPD{G2} = TabularCPD([0.9,0.1;0.1,0.9]);
@@ -44,16 +44,16 @@ dgm = DgmDist(graph,'CPDs', CPD,'infMethod', JtreeInfEng());
 
 evidence = {[50,50], [50], [60,60], [50,60]};
 for i=1:length(evidence)
-  ev = evidence{i};
-  if length(ev)==2
-    cond = [X2, X3];
-  else
-    cond = [X2];
-  end
-  %pG1(i) = sub(pmf(marginal(dgm, G1, cond, ev)),1); 
-  pG1a = marginal(dgm, G1, cond, ev); % a tabularFactor
-  pG1b = pmf(pG1a); % a vector of numbers
-  pG1(i) = pG1b(1); % probability in state 1 (healthy)
+    ev = evidence{i};
+    if length(ev)==2
+        cond = [X2, X3];
+    else
+        cond = [X2];
+    end
+    %pG1(i) = sub(pmf(marginal(dgm, G1, cond, ev)),1);
+    pG1a = marginal(dgm, G1, cond, ev); % a tabularFactor
+    pG1b = pmf(pG1a); % a vector of numbers
+    pG1(i) = pG1b(1); % probability in state 1 (healthy)
 end
 assert(approxeq(pG1, [0.9863 0.8946 0.0137 0.5]))
 
