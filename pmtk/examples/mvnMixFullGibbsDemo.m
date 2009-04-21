7b30d62fef86a8188ad24b4ebe7e6d2025b597ce
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-04-21 23:44:19 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@813 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/mvnMixFullGibbsDemo.m
+++ pmtk/examples/mvnMixFullGibbsDemo.m
@@ -1,6 +1,6 @@
 %% (Full) Gibbs sampling from a mixture of multivariate normals
 %#author Cody Severinski
-%#broken
+
 setSeed(0);
 % Set the number of clusters K
 % the number of observations n to generate in d dimensions,
@@ -15,50 +15,53 @@ X = mvnrnd(mu(p,:),Sigma(:,:,p));
 % Determine the true log probability of the data
 logprobtrue = 0;
 for j=1:K
-  logprobtrue = logprobtrue + sum(logprob(MvnDist(mu(j,:), Sigma(:,:,j)), X(p == j,:)));
+  logprobtrue = logprobtrue + log(sum(p==k) / n) + sum(logprob(MvnDist(mu(j,:), Sigma(:,:,j)), X(p == j,:)));
 end
 
 % specify the prior distribution to use.
 chosenPrior = MvnInvWishartDist('mu', mean(X), 'Sigma', diag(var((X))), 'dof', d + 1, 'k', 0.001);
-model = MvnMixDist('distributions',copy( MvnDist(zeros(d,1),diag(ones(d,1)), '-prior', chosenPrior), K,1) ) ;
-
-% Set the prior distribution on the mixing weights to be Dirichlet(1,..., 1)
-model.mixingWeights.prior = DirichletDist(ones(K,1));
+model = MixMvnGibbs('distributions',copy( MvnDist(zeros(d,1),diag(ones(d,1)), '-prior', chosenPrior), K,1) ) ;
 
 % Initiate the sampler
-mcmc = latentGibbsSample(model,X);
+dists = latentGibbsSample(model,X, 'verbose', true);
 
-% Post-process - will add in later
-%[permOut] = processLabelSwitch(model,mcmc,X);
+N = size(dists.latentDist.samples,1);
+latent = dists.latentDist.samples;
+mix = dists.mixDist.samples;
+musample = dists.muDist.samples;
+Sigmatmp = dists.SigmaDist.samples;
+Sigmasample = zeros(d,d,N,K);
 
-% Plot some interesting results - point estimates
-plot(mcmc.loglik, 'linewidth', 3);
-xlabel('Iteration (after accounting for burnin and thinning)');
-ylabel('log likelihood');
-title('log likelihood vs iteration for the Gibbs Sampler');
-
-adjrand = zeros(length(mcmc.loglik),1);
-rand = zeros(length(mcmc.loglik),1);
-for j=1:length(mcmc.loglik)
-[adjrand(j),rand(j),junk1,junk2] = valid_RandIndex(mcmc.latent(:,j),p);
+for s=1:N
+  for k=1:K
+    w = Sigmatmp(s,:,k);
+    Sigmasample(:,:,s,k) = reshape(w',d,d)'*reshape(w',d,d);
+  end
 end
 
-bestrand = mcmc.latent(:,argmax(rand));
-bestll = mcmc.latent(:,argmax(mcmc.loglik));
+loglik = zeros(N,1);
+
+for s=1:N
+  for k=1:K
+    model.distributions{k}.mu = musample(s,:,k);
+    model.distributions{k}.Sigma = Sigmasample(:,:,s,k);
+    loglik(s) = loglik(s) + log(mix(s,k)) + sum(logprob(model.distributions{k}, X(latent(s,:) == k,:)) );
+  end
+end
 
 colortrue = {'bo', 'go', 'ro', 'co', 'mo'};
-colorrand = {'bx', 'gx', 'rx', 'cx', 'mx'};
+colorestm = {'bx', 'gx', 'rx', 'cx', 'mx'};
+
+bestll = argmax(loglik);
+figure(); hold on;
+plot(loglik, 'linewidth', 3);
+line([1,N],[logprobtrue, logprobtrue], 'color', 'green', 'linewidth', 3);
+plot(bestll, loglik(bestll), 'ro', 'MarkerSize', 10);
+legend('MCMC log-liklihood', 'true log-likelihood', 'MAP estimate');
 
 figure(); hold on;
-for j=1:K
-    plot(X(p == j,1), X(p == j,2), colortrue{j}, 'linewidth', 3, 'MarkerSize', 10);
-    plot(X(bestrand == j,1), X(bestrand == j,2), colorrand{j}, 'linewidth', 3, 'MarkerSize', 10);
-end
-title('Real vs. Gibbs clustering for iteration achieving best rand index.')
-
-figure(); hold on;
-for j=1:K
-    plot(X(p == j,1), X(p == j,2), colortrue{j}, 'linewidth', 3, 'MarkerSize', 10);
-    plot(X(bestll == j,1), X(bestll == j,2), colorrand{j}, 'linewidth', 3, 'MarkerSize', 10);
+for k=1:K
+    plot(X(p == k,1), X(p == k,2), colortrue{k}, 'linewidth', 2, 'MarkerSize', 10);
+    plot(X(p == k,1), X(p == k,2), colorestm{k}, 'linewidth', 2, 'MarkerSize', 10);
 end
 title('Real vs. Gibbs clustering for iteration achieving best log likelihood')
