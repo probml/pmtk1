c481406e26bf4c54bd49f24b7a3243a348c1001f
mattdunham@yahoo.com <mattdunham@yahoo.com@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-04-22 18:39:59 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@819 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/data/setupMnist.m
+++ pmtk/data/setupMnist.m
@@ -5,10 +5,14 @@ if nargin < 2, Ntrain = 60000; end
 if nargin < 3, Ntest = 10000; end
 if nargin < 4, full  = false; end
 
-if(~exist('mnistAll.mat','file'))
-    error('Please download mnistAll.mat from http://www.cs.ubc.ca/~murphyk/pmtk/mnistALL.mat');    
-end    
-load mnistALL
+%if(~exist('mnistAll.mat','file'))
+%    error('Please download mnistAll.mat from http://www.cs.ubc.ca/~murphyk/pmtk/mnistALL.mat');    
+%end
+try
+    load mnistALL
+catch ME
+    error('Please download mnistAll.mat from http://www.cs.ubc.ca/~murphyk/pmtk/mnistALL.mat');     
+end
 Xtrain = reshape(mnist.train_images(:,:,1:Ntrain),28*28,Ntrain)';
 Xtest = reshape(mnist.test_images(:,:,1:Ntest),28*28,Ntest)';
 ytrain = (mnist.train_labels);
