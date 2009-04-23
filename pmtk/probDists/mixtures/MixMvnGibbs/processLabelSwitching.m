b7633b277aa173728e6b3ed71fbe5aefc1dfc0b0
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-04-23 19:26:11 +0000

    Changed the stopping criteria
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@831 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/probDists/mixtures/MixMvnGibbs/processLabelSwitching.m
+++ pmtk/probDists/mixtures/MixMvnGibbs/processLabelSwitching.m
@@ -37,11 +37,13 @@
       % perm will contain the permutation that minimizes step two of the algorithm
       % The permutations are as indices, is perm(1,1) indicates how we permute label 1 for iteration 1
       perm = bsxfun(@times,1:K,ones(N,K));
+      ident = bsxfun(@times, 1:K, ones(N,K));
       oldPerm = bsxfun(@times,-inf,ones(N,K));
       fixedPoint = false;
       % For tracking purposes; value is how many times we have done the algorithm (itr and k already taken)
       klscore = 0;
       value = 1;
+      fail = false;
       fprintf('Attempting to resolve label switching \n')
       while( ~fixedPoint )
         if(verbose), fprintf('(Run %d). Computing Q for iteration  ', value);end;
@@ -88,30 +90,38 @@
           end
         % find the optimal permutation for this iteration, and then store in loss vector
         [perm(itr,:), loss(itr)] = assignmentoptimal(kl);
-        for k=1:K
-          mu(itr,:,k) = mu(itr,:,perm(itr,k));
-          logconst(itr,k) = logconst(itr,perm(itr,k));
-          invS(:,:,itr,k) = invS(:,:,itr,perm(itr,k));
-        end
         end
         % KL loss is the sum of all the losses over all the iterations
         klscore(value) = mean(loss);
 
-        % Stopping criteria - what would be ideal is to have a vector of stopping criteria
-        % and have the user select the stopping criteria
-        % I'm thinking that we could pass this in as varargin, and then evaluate the chosen
-        % criteria after each run
-        %if( value > 2 && (all(all(perm == oldPerm)) || approxeq(klscore(value), klscore(value-1), 1/klscore(value), 1) || approxeq(klscore(value), klscore(value-2), 1/klscore(value), 1) ) )
-        if(all(all(perm == oldPerm)) || value >= maxitr)
-          fixedPoint = true;
-        end
-        oldPerm = perm;
         if(value == 1)
           deltakl = 0;
         else
           deltakl = klscore(value) - klscore(value - 1);
         end
         if(verbose),fprintf('KL Loss = %g.  Delta = %g \n',klscore(value), deltakl); end;
+        % Stopping criteria - what would be ideal is to have a vector of stopping criteria
+        % and have the user select the stopping criteria
+        % I'm thinking that we could pass this in as varargin, and then evaluate the chosen
+        % criteria after each run
+        %if( value > 2 && (all(all(perm == oldPerm)) || approxeq(klscore(value), klscore(value-1), 1/klscore(value), 1) || approxeq(klscore(value), klscore(value-2), 1/klscore(value), 1) ) )
+        if(value > 2 && (all(all(perm == ident)) || value >= maxitr || convergenceTest(klscore(value), klscore(value - 1))))
+          fixedPoint = true;
+        end
+        if(deltakl > 0)
+          warning('processLabelSwitching', 'Objective did not decrease.  Returning with last good permutation');
+          permOut = oldPerm;
+          fixedPoint = true;
+          fail = true;
+        end
+
+        if(fail ~= true)
+          for k=1:K
+            mu(itr,:,k) = mu(itr,:,perm(itr,k));
+            logconst(itr,k) = logconst(itr,perm(itr,k));
+            invS(:,:,itr,k) = invS(:,:,itr,perm(itr,k));
+          end
+        end
         value = value + 1;   
       end
       permOut = perm;
