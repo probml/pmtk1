feaa221a56135332fdaa4c2f6f19717182af5885
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-05-04 16:33:37 +0000

    Switched from export_fig to exportfig toolbox as it is more OS-friendly and produces comparable results
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@895 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/util/printPmtk/printPmtkFigures.m
+++ pmtk/util/printPmtk/printPmtkFigures.m
@@ -1,9 +1,10 @@
-function [] = doPrintPmtkFigures(filename)
+function [] = printPmtkFigures(filename)
   printFolder = '/ubc/cs/home/n/nevek/fig';
   % create an eps and pdf file for the figure
   pdfcrop;
-  exportfig(gcf, sprintf('%s/%s.eps', printFolder, filename), 'Resolution', 1200, 'Format', 'eps' );
-  exportfig(gcf, sprintf('%s/%s.pdf', printFolder, filename), 'Resolution', 1200, 'Format', 'pdf' );
+  opts = struct('Color', 'rgb', 'Resolution', 1200);
+  exportfig(gcf, sprintf('%s/%s.eps', printFolder, filename), opts, 'Format', 'eps' );
+  exportfig(gcf, sprintf('%s/%s.pdf', printFolder, filename), opts, 'Format', 'pdf' );
   %export_fig(fullfile(printFolder, sprintf('%s.pdf', filename)), '-pdf');
   %export_fig(fullfile(printFolder, sprintf('%s.eps', filename)), '-eps');
 end
