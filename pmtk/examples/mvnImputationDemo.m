493e960cd0976ac8c7ae82d324c6e162b9c86edf
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-05-08 17:05:37 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@912 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/mvnImputationDemo.m
+++ pmtk/examples/mvnImputationDemo.m
@@ -39,7 +39,7 @@ XhidImg(~missing) = 0;
 
 nr = 2; nc = 2;
 
-%{
+
 figure; 
 subplot(nr,nc,1); imagesc(Xfull); title('full data'); colorbar
 %subplot(nr,nc,2); imagesc(missing); title('missing pattern'); colorbar
@@ -47,20 +47,20 @@ subplot(nr,nc,2); imagesc(XmissImg); title('observed data'); colorbar
 subplot(nr,nc,3); imagesc(Ximpute); title('imputed mean'); colorbar
 subplot(nr,nc,4); imagesc(XhidImg); title('hidden truth'); colorbar
 %set(gcf,'position',[10 500 600 200])
-%}
 
-figure;
-subplot(nr,nc,1); hintonScale(Xfull, ones(n,d)); title('full data'); colorbar
-%subplot(nr,nc,2); hintonScale(missing, ones(n,d));title('missing pattern'); colorbar
-subplot(nr,nc,2); hintonScale(Xfull, 1-missing); title('observed data'); colorbar
-subplot(nr,nc,3); hintonScale(Ximpute, V); colormap('gray');
-title('imputed mean (color)/ variance (size)'); colorbar
-subplot(nr,nc,4); hintonScale(Xfull, missing); colormap('gray');
-title('hidden truth'); colorbar
-
-
+hintonScale({Xfull, ones(n,d)}, {'-title', 'full data'}, {Xfull, 1-missing}, {'-title', 'missing pattern'}, {Ximpute, V}, {'-title', 'observed data'}, {Xfull, missing}, {'-title', 'hidden truth'});
+%figure;
+%subplot(nr,nc,1); hintonScale(Xfull, ones(n,d)); title('full data');
+%%subplot(nr,nc,2); hintonScale(missing, ones(n,d));title('missing pattern'); colorbar
+%subplot(nr,nc,2); hintonScale(Xfull, 1-missing); title('observed data');
+%subplot(nr,nc,3); hintonScale(Ximpute, V); 
+%title('imputed mean (color)/ variance (size)');
+%subplot(nr,nc,4); hintonScale(Xfull, missing);
+%title('hidden truth');
 
 
+
+%{
 figure;
 subplot(nr,nc,1); hintonScale(Xfull, ones(n,d), '-map', gray);
 title('full data'); colorbar
@@ -70,7 +70,7 @@ subplot(nr,nc,3); hintonScale(Ximpute, V, '-map', 'gray');
 title('imputed mean (color)/ variance (size)'); colorbar
 subplot(nr,nc,4); hintonScale(Xfull, missing, '-map', 'gray');
 title('hidden truth'); colorbar
-
+%}
 
 
 
