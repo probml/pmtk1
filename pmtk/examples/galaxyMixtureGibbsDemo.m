368f9c2e71d0fc7273a17a94c8dab83653a39da3
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-05-20 15:58:51 +0000

    git-svn-id: https://pmtk.googlecode.com/svn/trunk@936 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/galaxyMixtureGibbsDemo.m
+++ pmtk/examples/galaxyMixtureGibbsDemo.m
@@ -15,21 +15,31 @@
   galaxies = galaxies / 1000; galaxies = galaxies(randperm(n));
 
   % specify the prior distribution to use.
-%  chosenPrior = MvnInvWishartDist('mu', mean(galaxies)', 'Sigma', diag(var((galaxies))) / K^(2/d), 'dof', d + 2, 'k', 0.01);
+% This is the MvnInvWishart equivalent parametrization suggested by
+%  @article{fearnhead2004pfm,
+%    title={{Particle filters for mixture models with an unknown number of components}},
+%    author={Fearnhead, P.},
+%    journal={Statistics and Computing},
+%    volume={14},
+%    number={1},
+%    pages={11--21},
+%    year={2004},
+%    publisher={Springer}
+%  }
   chosenPrior = MvnInvWishartDist('mu', 20, 'Sigma', 4, 'dof', 4, 'k', 1/15^2);
   model = MixMvnGibbs('distributions',copy( MvnDist('-mu', zeros(d,1),'-Sigma', diag(ones(d,1)), '-prior', chosenPrior), K,1) ) ;
-
-  % Set the prior distribution on the mixing weights to be Dirichlet(1,..., 1)
   model.mixingDistrib.prior = DirichletDist(ones(K,1));
-profile off; profile on;
   % Initiate the sampler
-  [model, latent] = fit(model, galaxies, '-method', 'collapsed', '-Nsamples', 500, '-Nburnin', 250, '-verbose', true);
-profile viewer
-  % Perform postprocessing on the labels
-  %[distsout, permOut] = processLabelSwitching(dists, galaxies);
+  method = {'full', 'collapsed'};
+  for m=1:length(method)
+    [fitted{m}, latent{m}] = fit(model, galaxies, '-method', method{m}, '-Nsamples', 1250, '-Nburnin', 250, '-verbose', true);
+  end
 
-  % New stuff here
-  %modelAvg = mean(model, distsout);
+  for m=1:length(method)
+    traceplot(fitted{m}); suptitle(sprintf('%s Gibbs sampling', method{m}));
+    convergencePlot(fitted{m}); suptitle(sprintf('%s Gibbs sampling', method{m}));
+  end
+
 
 
   
