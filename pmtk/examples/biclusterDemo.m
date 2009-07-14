c4e42d02bc945bce039271137dd80a9d6bf1bbea
severinski.cody <severinski.cody@d55ff168-9d76-11dd-b389-bb47fade112a>
2009-07-14 21:44:08 +0000

    Changed a few values (default thresholds, data matrix size, embedded bicluster size, etc.) to match those from the journal paper.  Also made some minor cosmetic changes to how the code is organized
    git-svn-id: https://pmtk.googlecode.com/svn/trunk@964 d55ff168-9d76-11dd-b389-bb47fade112a

--- pmtk/examples/biclusterDemo.m
+++ pmtk/examples/biclusterDemo.m
@@ -1,17 +1,22 @@
-cls; 
+%cls; 
 setSeed(0);
 nRow = 100;
 nCol = 30;
+nLevels = 3;
+
 
 %% First a really simple example
 fprintf('First discover a bicluster in a really simple example -- binary data\n')
 data = zeros(nRow, nCol);
+
+
 
 data(5:5:end, 5:5:end) = 1;
 patternRows = 5:5:size(data,1);
 patternCols = 5:5:size(data,2);
 
 %bicluster(data);
+setSeed(1);
 [dummyBcRow, dummyBcCol, traceDummyRow, traceDummyCol] = biclusterMultiple(data, '-trace', true);
 for c=1:size(traceDummyRow)
   figure(); plot(traceDummyRow{c}'); legend();
@@ -42,57 +47,66 @@ end;
 fprintf('\n\nNow we try a more difficult example, similar to the example from Fig. 2 in Shen et al (2003): Biclustering Microarray Data in Gibbs Sampling\n')
 
 clear data;
-nLevels = 3;
 data = unidrnd(nLevels, [nRow, nCol]);
 % now we embed the pattern
 patternRowSize = 25;
 patternColSize = 8;
 patternRows = randsample(nRow, patternRowSize);
 patternCols = randsample(nCol, patternColSize);
-fprintf('True rows: %s\n', mat2str(sort(patternRows')))
-fprintf('True columns: %s', mat2str(sort(patternCols')))
-sharp = 0.80;
+sharp = 0.90;
+nLevels = 3;
 p = [sharp, normalize(ones(1,nLevels-1))*(1-sharp)]; % a sharp multinomial distribution
 p = perms(p);
 p = p(unidrnd(size(p,1), patternColSize, 1),:);
+
+fprintf('True rows: %s\n', mat2str(sort(patternRows')))
+fprintf('True columns: %s\n', mat2str(sort(patternCols')))
 for k=1:patternColSize
   data(patternRows,patternCols(k)) = sampleDiscrete(p(k,:), patternRowSize, 1);
 end
 
 %bicluster(data);
+setSeed(2);
 [uniBcRow, uniBcCol] = biclusterMultiple(data);
 if(doPrintPmtk)
   figure(1);
   printPmtkFiguresKPM('uniBiclusterData'); 
 end;
 
-%{ Not working yet
-fprintf('\n\nNow multiple biclusters\n')
-clear data
-nLevels = 3;
+fprintf('\n\nNow multiple biclusters on a larger data matrix\n')
+clear data patternRows patternCols
+nRow = 200;
+nCol = 40;
+sharp = 0.90;
+p = [sharp, normalize(ones(1,nLevels-1))*(1-sharp)]; % a sharp multinomial distribution
+p = perms(p);
+p = p(unidrnd(size(p,1), patternColSize, 1),:);
 data = unidrnd(nLevels, [nRow, nCol]);
 truecount = 3;
-patternRowSizeVec = [25, 20, 10];
-patternRowStart = [1, 30, 60];
-patternColSizeVec = [8, 5, 2];
-patternColStart = [1, 10, 18];
+patternRowSizeVec = [40, 25, 35];
+%patternRowStart = [1, 30, 60];
+patternColSizeVec = [7, 2, 8];
+%patternColStart = [1, 10, 18];
 
+%rowProb = normalize(ones(1, nRow));
+%colProb = normalize(ones(1, nRow));
+%patternRowsVec = sample(rowProb, sum(patternRowSizeVec));
+%patternColsVec = sample(colProb, sum(patternColSizeVec));
 for j=1:truecount
   % now we embed the pattern
   patternRowSize = patternRowSizeVec(j);
   patternColSize = patternColSizeVec(j);
 
-  patternRows = patternRowStart(j):(patternRowStart(j) + patternRowSize - 1);
-  patternCols = patternColStart(j):(patternColStart(j) + patternColSize - 1);
-  %fprintf('True rows: %s\n', mat2str(sort(patternRows')))
-  %fprintf('True columns: %s', mat2str(sort(patternCols')))
-%  p = [sharp, normalize(ones(1,nLevels-1))*(1-sharp)]; % a sharp multinomial distribution
-%  p = perms(p);
-%  p = p(unidrnd(size(p,1), patternColSize, 1),:);
+  patternRows{j} = randsample(nRow, patternRowSize);
+  patternCols{j} = randsample(nCol, patternColSize);
+  fprintf('True rows: %s\n', mat2str(sort(patternRows{j}')))
+  fprintf('True columns: %s\n', mat2str(sort(patternCols{j}')))
+%  patternRows = patternRowStart(j):(patternRowStart(j) + patternRowSize - 1);
+%  patternCols = patternColStart(j):(patternColStart(j) + patternColSize - 1);
   for k=1:patternColSize
-    data(patternRows, patternColStart(j) + k) = sampleDiscrete(p(j,:), patternRowSize, 1);
+    data(patternRows{j}, patternCols{j}(k)) = sampleDiscrete(p(j,:), patternRowSize, 1);
   end
 end
 
+setSeed(3);
 [multBcRow, multBcCol] = biclusterMultiple(data);
-%}
